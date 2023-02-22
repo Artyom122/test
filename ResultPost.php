944f81cc0063c20c6757b0fe1e6a66b6c5154a76
Artyom122 <126030058+Artyom122@users.noreply.github.com>
2023-02-22 04:48:20 +0300

    Update and rename Registr.txt to ResultPost.php

---
+++ ResultPost.php
@@ -0,0 +1,11 @@
+<?php
+    $name = $_POST['username'];
+    $email = $_POST['email'];
+
+    if(trim(name) = "")
+        echo "Некорректное имя пользователя";
+    else if (strlen(trim($name)) <= 1)
+        echo "Такого имени не существует";
+    else if (trim($email) == "")
+        echo "Заполните поле"
+     else if($trim=()
